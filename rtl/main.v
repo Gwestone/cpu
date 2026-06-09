6e78f77b57e0cfe3298e818b5c1b319c3f78802f
Your Name <you@example.com>
2026-06-09 12:25:35 +0300

    basic cpu operational

--- rtl/main.v
+++ rtl/main.v
@@ -1,23 +1,91 @@
-`timescale 1ns / 1ps
+module main(
+    input wire clk,
+    input wire reset,
+    output reg [63:0] raddr,
+    input wire [63:0] rdata,
+    output reg [63:0] waddr,
+    output reg [63:0] wdata);
 
-module main(clk, data, reset, message);
-
-    input wire clk;
-    input wire reset;
-    input wire [7:0] data;
-
-    output reg [7:0] message;
-
-    reg [7:0] message_reg;
+    reg [63:0] pc;
+    reg [6:0] opcode;
+    reg [2:0] func3;
+    reg [6:0] func7;
+    reg [4:0] rs1;
+    reg [4:0] rs2;
+    reg [4:0] rd;
+    reg [11:0] imm;
+    reg [63:0] registers [0:31];
 
     always @(posedge clk) begin
-        if (reset) begin
-            message_reg <= 0;
+        if(reset) begin
+            pc <= 0;
+            for (integer i = 0; i < 32; i = i + 1) begin
+                registers[i] <= 0;
+            end
+            opcode <= 0;
+            func3 <= 0;
+            func7 <= 0;
+            rs1 <= 0;
+            rs2 <= 0;
+            rd <= 0;
+            imm <= 0;
+            raddr <= 0;
         end else begin
-            message_reg <= data;
+            opcode <= rdata[6:0];
+            func3 <= rdata[14:12];
+            func7 <= rdata[31:25];
+            rs1 <= rdata[19:15];
+            rs2 <= rdata[24:20];
+            rd <= rdata[11:7];
+            imm <= rdata[31:20];
+            raddr <= pc;
+            case(opcode)
+                7'b00: pc <= pc + 4;
+                7'b0010011: begin
+                    case (func3)
+                        3'b000: registers[rd] <= registers[rs1] + 64'(imm);
+                        3'b001: ;
+                        3'b010: ;
+                        3'b011: ;
+                        3'b100: ;
+                        3'b101: ;
+                        3'b110: ;
+                        3'b111: ;
+                    endcase
+                    pc <= pc + 4;
+                end
+                7'b0100011: begin
+                    case (func3)
+                        3'b000: ;
+                        3'b001: ;
+                        3'b010: ;
+                        3'b011: begin
+                            waddr <= registers[rs1] + 64'(imm);
+                            wdata <= registers[rs2];
+                        end
+                        3'b100: ;
+                        3'b101: ;
+                        3'b110: ;
+                        3'b111: ;
+                    endcase
+                    pc <= pc + 4;
+                end
+                7'b1100111: begin
+                    case (func3)
+                        //TODO add rd, when i find out what it does
+                        3'b000: pc <= registers[rs1] + 64'(imm);
+                        3'b001: ;
+                        3'b010: ;
+                        3'b011: ;
+                        3'b100: ;
+                        3'b101: ;
+                        3'b110: ;
+                        3'b111: ;
+                    endcase
+                end
+                default: pc <= pc + 4;
+            endcase
         end
     end
-
-    assign message = message_reg;
 
 endmodule
--- tests/main_tb.py
+++ tests/main_tb.py
@@ -1,45 +1,89 @@
+import copy
+import struct
+from os import getcwd, path
+
 import cocotb
-from cocotb.triggers import FallingEdge, Timer
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+bin_file_path = path.join(getcwd(), "bin/src/firmware.bin")
+bin_file = open(bin_file_path, "rb")
+binary_blob = bin_file.read()
 
 
-async def generate_clock(dut):
-    """Generate clock pulses."""
+def read_full(address, array):
+    return (
+        (array[address] << 0)  # first byte = least significant
+        | (array[address + 1] << 8)
+        | (array[address + 2] << 16)
+        | (array[address + 3] << 24)  # last byte = most significant
+    )
 
-    for _ in range(10):
-        dut.clk.value = 0
-        await Timer(1, unit="ns")
-        dut.clk.value = 1
-        await Timer(1, unit="ns")
+
+def make_blob(words):
+    blob = bytearray()
+    for w in words:
+        blob += struct.pack("<I", w)
+    return blob
+
+
+async def reset(dut):
+    dut.reset.value = 1
+    await ClockCycles(dut.clk, 1)
+    dut.reset.value = 0
+    await ClockCycles(dut.clk, 1)
 
 
 @cocotb.test()
-async def my_first_test(dut):
+async def my_test(dut):
     """Try accessing the design."""
+    cocotb.start_soon(Clock(dut.clk, 2).start())
+    await reset(dut)
 
-    dut.data.value = 0b0000000
-    dut.reset.value = 0
+    blob_copy = copy.copy(binary_blob)
+    # addi x0, x0, 0
+    program = [0x00000013] * 40
+    blob = make_blob(program)
 
+    print("Entire program blob: " + " ".join(hex(i) for i in blob))
     for _ in range(10):
-        dut.clk.value = 0
-        await Timer(1, unit="ns")
-        dut.clk.value = 1
-        await Timer(1, unit="ns")
-
-    cocotb.log.info("response is %s", dut.message.value)
-    assert dut.message.value == dut.data.value
+        address_to_read = dut.raddr.value
+        print("Reading address: " + hex(address_to_read))
+        dut.rdata.value = blob[address_to_read]
+        await ClockCycles(dut.clk, 1)
 
 
 @cocotb.test()
-async def my_second_test(dut):
-    """Try accessing the design."""
+async def immediate_add_operation(dut):
+    """Try performing some computation."""
+    cocotb.start_soon(Clock(dut.clk, 2).start())
+    await reset(dut)
 
-    dut.data.value = 0b0000000
-    dut.reset.value = 0
+    blob_copy = copy.copy(binary_blob)
+    program = [
+        0x00000000,  # nop
+        0x00200013,  # addi x0, x0, 2
+        0x00200013,  # addi x0, x0, 2
+        0x04008093,  # addi x1, x1, 64
+        0x0000B023,  # sd x0, 0(x1)
+        # end loop to prevent crash
+        0x01400093,  # addi x1, x0, 24
+        0x00008067,  # jalr x0, 0(x1) (infinite loop)
+        0x00000000,  # nop
+        0x00000000,  # nop
+        0x00000000,  # nop
+    ]
 
-    cocotb.start_soon(generate_clock(dut))  # run the clock "in the background"
+    blob = make_blob(program)
 
-    await Timer(10, unit="ns")  # wait a bit
-    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
+    print("Entire program blob: " + " ".join(hex(i) for i in blob))
 
-    cocotb.log.info("response is %s", dut.message.value)
-    assert dut.message.value == dut.data.value
+    for _ in range(100 * 4):
+        addr = int(dut.raddr.value)
+        data = read_full(addr, blob)
+        print("Reading address: " + hex(addr) + " (value: " + hex(data) + ")")
+        dut.rdata.value = data
+        await ClockCycles(dut.clk, 1)
+
+    assert dut.waddr.value == 0x40
+    assert dut.wdata.value == 0x04
